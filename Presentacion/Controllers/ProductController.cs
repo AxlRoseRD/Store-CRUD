1a92d61b7b8b9427110196a2c5cc420049e1d007
Axcell Espinal <61571507+AxlRoseRD@users.noreply.github.com>
2021-08-18 04:00:18 -0400

    Add files via upload

---
+++ Presentacion/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entities;
+using Business;
+using Business.Business_Maintenance;
+
+namespace Presentacion.Controllers
+{
+    public class ProductController : Controller
+    {
+        Product_Business_Data BusinessProduct = new Product_Business_Data();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+    }
+}
