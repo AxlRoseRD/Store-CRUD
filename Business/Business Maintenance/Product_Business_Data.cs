ab09b4a63bc305538a00584bbf65b8c900f9a9a2
Axcell Espinal <61571507+AxlRoseRD@users.noreply.github.com>
2021-08-18 04:02:43 -0400

    Add files via upload

---
+++ Business/Business Maintenance/Product_Business_Data.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data_Base;
+using Entities;
+
+namespace Business.Business_Maintenance
+{    
+   public class Product_Business_Data
+    {
+        ProductData data = new ProductData();
+
+        public void insertBusinessData(Products product) 
+        {            
+            data.insertData(product);
+        }
+    }
+}
+
+/*
+|--------------------------------------|
+|-----A--------X-------X----L----------|
+|----A A---------X---X------L----------|
+|---A---A----------X--------L----------|
+|--A A A A-------X---X------L----------|
+|-A-------A----X-------X----L----------| 
+|A---------A X-----------X -L-L-L-L-L--|
+|--------------------------------------|
+*/
---
+++ Business/Business Maintenance/Vendor_Business_Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using Data_Base;
+namespace Business.Business_Maintenance
+{
+   public class Vendor_Business_Data
+    {
+    }
+}
